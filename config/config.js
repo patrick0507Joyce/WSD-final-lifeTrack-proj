1bde2de04af78b3c5914a4cda529d1146a45de4f
Xiaohu Shu <sxh20150609@gmail.com>
2020-12-10 22:33:35 +0200

    test for db

--- config/config.js
+++ config/config.js
@@ -6,6 +6,7 @@ if (Deno.env.get('TEST_ENVIRONMENT')) {
     config.database = {};
   } else {
     const CONCURRENT_CONNECTIONS = 1;
+    console.log("db url is: ", Deno.env.toObject().DATABASE_URL);
     config.database = new Pool(
       Deno.env.toObject().DATABASE_URL,
       CONCURRENT_CONNECTIONS
