af2f496ce4194d9b81db08db1a2a39a14e5291aa
Xiaohu Shu <sxh20150609@gmail.com>
2020-12-11 00:48:48 +0200

    remove unnecessary connect()

--- database/database.js
+++ database/database.js
@@ -8,7 +8,6 @@ const pool = new Pool(config.database, CONCURRENT_CONNECTIONS);
 const executeQuery = async (query, ...params) => {
   const client = await pool.connect();
   try {
-    await client.connect();
     return await client.query(query, ...params);
   } catch (e) {
     console.log(e);
