a95352277887d1cf91ee8eeb2a41958949fda39d
Xiaohu Shu <sxh20150609@gmail.com>
2020-12-11 00:33:23 +0200

    use cloud db

--- config/config.js
+++ config/config.js
@@ -1,21 +1,18 @@
-import { Pool } from "../deps.js";
-
 let config = {};
 
-if (Deno.env.get('TEST_ENVIRONMENT')) {
+if (Deno.env.toObject().DATABASE_URL) {
+  const DATABASE_URL = Deno.env.toObject().DATABASE_URL;
+  config.database = DATABASE_URL;
+} else if (Deno.env.get('TEST_ENVIRONMENT')) {
     config.database = {};
   } else {
-    const CONCURRENT_CONNECTIONS = 2;
-    config.database = new Pool(
-      {
+    config.database = {
         hostname: "hattie.db.elephantsql.com",
         database: "ejkihkqn",
         user: "ejkihkqn",
         password: "VQERJAuPVbPQj-xNK52wVKriXU8GixbE",
         port: 5432,
-      },
-      CONCURRENT_CONNECTIONS
-    );
+      };
   }
 
 export { config }; 
--- database/database.js
+++ database/database.js
@@ -1,12 +1,14 @@
 import { config } from "../config/config.js";
+import { Pool } from "../deps.js";
 
-const getClient = async () => {
-  return await config.database.connect();
-};
+
+const CONCURRENT_CONNECTIONS = 2;
+const pool = new Pool(config.database, CONCURRENT_CONNECTIONS);
 
 const executeQuery = async (query, ...params) => {
-  const client = await getClient();
+  const client = await pool.connect();
   try {
+    await client.connect();
     return await client.query(query, ...params);
   } catch (e) {
     console.log(e);
