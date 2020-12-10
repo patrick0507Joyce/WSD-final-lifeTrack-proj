386f665063e9bebc92cd4e92e66c761344ab0b6a
Xiaohu Shu <sxh20150609@gmail.com>
2020-12-10 23:17:30 +0200

    try local db

--- config/config.js
+++ config/config.js
@@ -3,6 +3,13 @@ let config = {};
 if (Deno.env.toObject().DATABASE_URL) {
   const DATABASE_URL = Deno.env.toObject().DATABASE_URL;
   config.database = DATABASE_URL;
+  config.database = {
+    hostname: "hattie.db.elephantsql.com",
+    database: "ejkihkqn",
+    user: "ejkihkqn",
+    password: "VQERJAuPVbPQj-xNK52wVKriXU8GixbE",
+    port: 5432,
+  };
 } else if (Deno.env.get("TEST_ENVIRONMENT")) {
   config.database = {};
 } else {
